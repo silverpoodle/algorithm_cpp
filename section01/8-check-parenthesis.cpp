0eab3f55592889d5c8d6ddd791b87ca5b767e2ac
Jungin <jungin.dylan@gmail.com>
2024-07-31 23:00:53 +0900

    240731

--- section01/8-check-parenthesis.cpp
+++ section01/8-check-parenthesis.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<stack>
 
 using namespace std;
 
@@ -7,16 +8,28 @@ using namespace std;
 int main() {
 
     string input, output="";
+    stack<char> s;
 
     cin >> input;
 
     for(char c : input) {
-        if(c != ' ') {
-            output += tolower(c);
+        if(c == '(') {
+            s.push(c);
+        } else if (c == ')') {
+            if(s.empty()) {
+                cout << "NO" << endl;
+                return 0;
+            }
+             else 
+                s.pop();
         }
     }
 
-    cout << output << endl;
+    if(s.empty()) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
 
     return 0;
 }
---
+++ section01/9-find-all-divisors.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+/* 9. 자연수 N 이 입력되면 1~N 각 숫자의 약수의 개수를 모두 출력하시오. (5 <= N <= 50,000) */
+/* 시간 제한 1초 */
+int main() {
+
+    int n;
+    cin >> n;
+
+    
+
+    for(int i = 1 ; i <= n ; i++) {
+
+    }
+    
+    return 0;
+}
