17420b1a0aa390ad53db0ba2353acfe91db7d2d8
silverpoodle <jungin.dylan@gmail.com>
2025-02-16 17:23:52 +0900

    250216

--- baekjoon/1929-find-prime.java
+++ baekjoon/1929-find-prime.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.math.*;
 
 /*
 M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
@@ -14,19 +13,31 @@ M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오
 
 
 class Main {
-    static int N, M;
-
-    
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = br.read();
-        M = br.read();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = N ; i < M ; i++) {
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        boolean[] isPrime = new boolean[N+1];
+        
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        
+        for(int i = 2; i <= Math.sqrt(N); i++) {
+            if(isPrime[i]) {
+                for(int j = i*i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
         }
-
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = N ; i <= M ; i++) {
+            if(isPrime[i]) sb.append(i).append("\n");
+        }
+        
+        System.out.print(sb);
     }
 }
-
