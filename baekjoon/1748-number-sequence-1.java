152dec7aca02db81aec2585d5b44ba4eddf07874
Jungin <jungin.dylan@gmail.com>
2025-02-24 22:23:37 +0900

    250224

---
+++ baekjoon/1748-number-sequence-1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+/*
+1부터 N까지의 수를 이어서 쓰면 다음과 같이 새로운 하나의 수를 얻을 수 있다.
+1234567891011121314151617181920212223...
+이렇게 만들어진 새로운 수는 몇 자리 수일까? 이 수의 자릿수를 구하는 프로그램을 작성하시오.
+
+- 입력
+    첫째 줄에 N(1 ≤ N ≤ 100,000,000)이 주어진다.
+
+- 출력
+    첫째 줄에 새로운 수의 자릿수를 출력한다.
+
+*/
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        
+
+    }
+
+}
