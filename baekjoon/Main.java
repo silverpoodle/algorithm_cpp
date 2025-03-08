ed910ed64c57785660dec741f05da839e78d2c3c
Jungin <jungin.dylan@gmail.com>
2025-03-08 23:57:07 +0900

    250308

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -33,7 +33,7 @@ import java.io.*;
 
 
 */
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int T = Integer.parseInt(br.readLine());
--- section01/16-anagram.java
+++ section01/16-anagram.java
@@ -15,6 +15,75 @@ class Main {
     static String str2;
     static boolean used[];
 
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     str1 = sc.nextLine();
+    //     str2 = sc.nextLine();
+
+    //     int N = str1.length();
+
+    //     if(N != str2.length()) {
+    //         System.out.println("NO");
+    //         return;
+    //     }
+
+    //     used = new boolean[N];
+    //     boolean found;
+
+    //     for(int i = 0; i < N ;i++) {
+    //         found = false;
+    //         for(int j = 0 ; j < N ; j++) {
+    //             if(str2.charAt(j) == str1.charAt(i) && !used[j]) {
+    //                 used[j] = true;
+    //                 found = true;
+    //                 break;
+    //             }
+    //         }
+    //         if(!found) {
+    //             System.out.println("NO");
+    //             return;
+    //         }
+    //     }
+
+    //     System.out.println("YES");
+    //     return;
+    // }
+
+    //HashMap O(N)
+    // public static void main(String[] args) {
+
+    //     Scanner sc = new Scanner(System.in);
+
+    //     str1 = sc.nextLine();
+    //     str2 = sc.nextLine();
+
+    //     int N = str1.length();
+
+    //     if(N != str2.length()) {
+    //         System.out.println("NO");
+    //         return;
+    //     }
+    //     HashMap<Character, Integer> charCnt = new HashMap<>();
+
+    //     for(int i = 0 ; i < N ; i++) 
+    //         charCnt.put(str1.charAt(i), charCnt.getOrDefault(i,0)+1);
+        
+    //     for(int i = 0 ; i < N ; i++) {
+    //         if(!charCnt.containsKey(str2.charAt(i)) || charCnt.get(str2.charAt(i)) == 0) {
+    //             System.out.println("NO");
+    //             return;
+    //         }
+    //         charCnt.put(str2.charAt(i), charCnt.get(str2.charAt(i))-1);
+    //     }
+
+    //     System.out.println("YES");
+    //     return;
+    // }
+
+
+
+    //Alphabet 배열
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -28,25 +97,19 @@ class Main {
             return;
         }
 
-        used = new boolean[N];
-        boolean found;
+        int[] count = new int[52];
 
-        for(int i = 0; i < N ;i++) {
-            found = false;
-            for(int j = 0 ; j < N ; j++) {
-                if(str2.charAt(j) == str1.charAt(i) && !used[j]) {
-                    used[j] = true;
-                    found = true;
-                    break;
-                }
-            }
-            if(!found) {
+        for(int i = 0 ; i < N ; i++) count[str1.charAt(i) - 'a']++;
+
+        for(int i = 0 ; i < N ; i++) {
+            count[str2.charAt(i) - 'a']--;
+            if(count[str2.charAt(i) - 'a'] < 0) {
                 System.out.println("NO");
                 return;
             }
         }
-
         System.out.println("YES");
         return;
     }
+
 }
